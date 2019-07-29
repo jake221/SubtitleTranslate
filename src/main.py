a439334fec9f33d535c8e1f9d22a926d82a972e6
jake221 <550596645@qq.com>
2019-07-29 10:57:11 +0800

    add obtain dialog index method

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 from src.utils import translate_and_compose
 from src.utils import read_txt
 
-input_srt_file = "../input_srt/training.srt"
-input_txt_file = '../input_txt/training.txt'
-output_srt_file = "../output_srt/training_en.srt"
+input_srt_file = "../input_srt/wengkai.srt"
+input_txt_file = '../input_txt/wengkai.txt'
+output_srt_file = "../output_srt/wengkai_en.srt"
 #
 # read plain text translation of the video
 plain_txt = read_txt(input_txt_file)
 
 # Translate the subtitle into English
 # save both English and Chinese to the output input_srt file
-translate_and_compose(input_srt_file, output_srt_file, plain_txt, 'zh-CN', 'en', encoding='UTF-8-sig', space=True)
-
-# translate_and_compose(input_srt_file, output_srt_file, plain_txt, 'zh-CN', 'en', encoding='UTF-8-sig', space=True, both=False)
+# translate_and_compose(input_srt_file, output_srt_file, plain_txt, 'zh-CN', 'en', encoding='UTF-8-sig', space=True)
+#
+translate_and_compose(input_srt_file, output_srt_file, plain_txt, 'zh-CN', 'en', encoding='UTF-8-sig', space=True, both=False)
